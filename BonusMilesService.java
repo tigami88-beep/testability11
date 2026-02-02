0bb310a052f3399f64308425b31600b5c6ad0845
tigami88-beep <tigami88@gmail.com>
2026-02-02 21:41:28 +0300

    Initial commit

--- BonusMilesService.java
+++ BonusMilesService.java
@@ -1,6 +1,9 @@
 public class BonusMilesService {
-    public int calculate(int price) {
-        // Пример простого правила: 1 миля за каждые 20 единиц цены
-        return price / 20;
+    public int calculate(int ticketPrice) {
+        if (ticketPrice <= 0) {
+            return 0;
+        }
+        // 1 миля за каждые 20 рублей
+        return ticketPrice / 20;
     }
 }
